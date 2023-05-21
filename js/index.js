793c77716fb56a0c99be94fb304bd3e2b1deb364
RobinLe <web@robinle.de>
2023-05-21 22:41:28 +0200

    pause "globally" on SPACE down

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
 let oInfobox;
 let btnOpen;
+let oAudio;
 
 function init()
 {
 	oInfobox     = document.getElementById("infobox");
 	btnOpen      = document.getElementById("infobox-openbtn");
+	oAudio       = document.querySelector("audio");
 	let btnClose = document.getElementById("infobox-closebtn");
 	
-	btnClose.addEventListener("click", onClose);
-	btnOpen.addEventListener("click", onOpen);
+	document.addEventListener("keydown", onKeyDown, false);
+	btnClose.addEventListener("click",   onClose);
+	btnOpen .addEventListener("click",   onOpen);
 }
 
+// collapse the infobox
 function onClose()
 {
 	oInfobox.style.display = "none";
@@ -18,8 +22,24 @@ function onClose()
 	
 }
 
+// expand the infobox
 function onOpen()
 {
 	oInfobox.style.display = "flex";
 	btnOpen.style.display  = "none";
+}
+
+// enable the user to "globally" pause with SPACE
+function onKeyDown(e)
+{
+	if (e.target === oAudio)
+		return; // is on audio element anyways
+	
+	if (e.key !== ' ')
+		return; // is not SPACE key
+	
+	if (oAudio.paused)
+		oAudio.play();
+	else
+		oAudio.pause();
 }
